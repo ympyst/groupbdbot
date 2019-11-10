56d219c01c975a4650ceb530df1f25eaa9f057d2
Polina Saarinen <saarinen@tutu.ru>
2019-11-10 23:48:23 +0300

    Use environment variables for port and token. Added Update struct

--- server.go
+++ server.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
+    "io/ioutil"
     "net/http"
+    "os"
 )
 
-func processUpdate(w http.ResponseWriter, req *http.Request) {
+type Update struct {
+    Id   int  `json:"update_id"`
+    Message string `json:"message"`
+}
 
+func processUpdate(w http.ResponseWriter, req *http.Request) {
+    defer req.Body.Close()
+    body, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+
+    var upd Update
+    err = json.Unmarshal(body, &upd)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+
+    fmt.Println(upd)
     fmt.Fprintf(w, "Hello\n")
 }
 
 func main() {
+    token := os.Getenv("TOKEN")
+    http.HandleFunc("/" + token, processUpdate)
 
-    http.HandleFunc("/803026579:AAEVPiHB9B3c5V63vvIwkLCFQZC68h5wTZo", processUpdate)
-
-    http.ListenAndServe(":8090", nil)
+    port := os.Getenv("PORT")
+    fmt.Printf("🔛 Now listening port %v", port)
+    err := http.ListenAndServe(":" + port, nil)
+    if err != nil {
+        panic(err)
+    }
 }
 
